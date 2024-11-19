392b079cbe73367e390365bc6cb58edfae28ea21
Sayantan Singha <sayantan.singha@webskitters.in>
2024-11-19 12:01:02 +0530

    Revert "changes"
    This reverts commit df271f3b5565afe8fb7640b54e25ab2a2ae9bcbd.

--- backend/app/interfaces/product.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IProduct {
-    name: string,
-    price: number,
-    description: string,
-    images: Array<string>,
-    product_type: 'New' | 'Used',
-    status?: 'Active' | 'Deactive'
-}
