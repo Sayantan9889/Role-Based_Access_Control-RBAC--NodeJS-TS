b83d2e0391b4a0aa0b7c395548919c87864e432b
Sayantan9889 <sayantan.silda98@gmail.com>
2024-11-23 01:21:19 +0530

    product image delete fix

--- app/controllers/product.controller.ts
+++ app/controllers/product.controller.ts
@@ -3,11 +3,12 @@ import { IProduct } from "../interfaces/product.interface";
 import { Request, Response } from "express";
 import { unlink } from "fs";
 import path from "path";
+// import { error } from "console";
 
 class ProductController {
     async createProduct(req: Request, res: Response): Promise<any> {
         try {
-            const body:IProduct = req.body;
+            const body: IProduct = req.body;
 
             const files = req.files;
 
@@ -30,16 +31,16 @@ class ProductController {
 
             const product = new productModel(body);
             // await product.validate();
-            const savedProduct:IProduct = await product.save();
+            const savedProduct: IProduct = await product.save();
 
-            res.status(200).json({
+            return res.status(200).json({
                 status: 200,
                 message: "Product created successfully!",
                 data: savedProduct,
             });
         } catch (error: any) {
             console.error("error: ", error);
-            res.status(500).json({
+            return res.status(500).json({
                 status: 500,
                 message: "Invalid product data!",
                 error,
@@ -49,15 +50,15 @@ class ProductController {
 
     async getAllProducts(req: Request, res: Response): Promise<any> {
         try {
-            const products:Array<IProduct> = await productModel.find({status: "Active"});
-            res.status(200).json({
+            const products: Array<IProduct> = await productModel.find({ status: "Active" });
+            return res.status(200).json({
                 status: 200,
                 message: "Products fetched successfully!",
                 data: products,
             });
         } catch (error: any) {
             console.error("error: ", error);
-            res.status(500).json({
+            return res.status(500).json({
                 status: 500,
                 message: "Failed to fetch products!",
                 error
@@ -67,15 +68,15 @@ class ProductController {
 
     async getProductById(req: Request, res: Response): Promise<any> {
         try {
-            const productId:string = req.params.id;
-            const product = await productModel.findOne({_id: productId, status: "Active"});
+            const productId: string = req.params.id;
+            const product = await productModel.findOne({ _id: productId, status: "Active" });
             if (!product) {
                 return res.status(404).json({
                     status: 404,
                     message: "Product not found!",
                 });
             }
-            res.status(200).json({
+            return res.status(200).json({
                 status: 200,
                 message: "Product fetched successfully!",
                 data: product,
@@ -83,7 +84,7 @@ class ProductController {
 
         } catch (error: any) {
             console.error("error: ", error);
-            res.status(500).json({
+            return res.status(500).json({
                 status: 500,
                 message: "Failed to fetch product!",
                 error,
@@ -93,7 +94,7 @@ class ProductController {
 
     async updateProduct(req: Request, res: Response): Promise<any> {
         try {
-            const productId:string = req.params.id;
+            const productId: string = req.params.id;
 
             const existingProduct = await productModel.findById(productId);
             if (!existingProduct) {
@@ -103,7 +104,7 @@ class ProductController {
                 });
             }
 
-            const body:IProduct = {
+            const body: IProduct = {
                 name: req.body.name || existingProduct.name,
                 price: req.body.price || existingProduct.price,
                 description: req.body.description || existingProduct.description,
@@ -123,7 +124,7 @@ class ProductController {
                 existingProduct.images.forEach((imagePath: string) => {
                     const imageName: string | undefined = imagePath.split('/').pop();
                     if (imageName) {
-                        const imagePathToDelete:string = path.join(__dirname, '../../uploads', imageName);
+                        const imagePathToDelete: string = path.join(__dirname, '../../uploads', imageName);
                         unlink(imagePathToDelete, (err) => {
                             if (err) {
                                 console.error(`Error deleting image: ${imagePathToDelete} - ${err.message}`);
@@ -139,14 +140,14 @@ class ProductController {
 
             // console.log("body: ", body);
             const updatedProduct = await productModel.findByIdAndUpdate(productId, body, { new: true });
-            res.status(200).json({
+            return res.status(200).json({
                 status: 200,
                 message: "Product updated successfully!",
                 data: updatedProduct,
             });
         } catch (error: any) {
             console.error("error: ", error);
-            res.status(500).json({
+            return res.status(500).json({
                 status: 500,
                 message: "Failed to update product!",
                 error,
@@ -157,8 +158,8 @@ class ProductController {
 
     async softyDeleteProduct(req: Request, res: Response): Promise<any> {
         try {
-            const productId:string = req.params.id;
-            const existingProduct:IProduct|null = await productModel.findById(productId);
+            const productId: string = req.params.id;
+            const existingProduct: IProduct | null = await productModel.findById(productId);
             if (!existingProduct) {
                 return res.status(404).json({
                     status: 404,
@@ -166,18 +167,62 @@ class ProductController {
                 });
             }
 
-            await productModel.findByIdAndUpdate(productId, {status: 'Deactive'});
-            res.status(200).json({
+            await productModel.findByIdAndUpdate(productId, { status: 'Deactive' });
+            return res.status(200).json({
                 status: 200,
                 message: "Product deleted successfully!",
             });
-            
-        } catch (err:any) {
+
+        } catch (err: any) {
             console.error("err: ", err);
-            res.status(500).json({
+            return res.status(500).json({
                 status: 500,
                 message: "Failed to delete product!",
-                error:err,
+                error: err,
+            });
+        }
+    }
+
+    async deleteProductImage(req: Request, res: Response): Promise<any> {
+        try {
+            const productId: string = req.params.id;
+            const imagePathToDelete: string = req.body.image;
+            const existingProduct: IProduct | null = await productModel.findById(productId);
+            if (!existingProduct) {
+                return res.status(404).json({
+                    status: 404,
+                    message: "Product not found!",
+                });
+            }
+            if (existingProduct.images.includes(imagePathToDelete)) {
+                existingProduct.images = existingProduct.images.filter((image: string) => image !== imagePathToDelete);
+                await productModel.findByIdAndUpdate(productId, { images: existingProduct.images });
+                const imageName: string = imagePathToDelete.split('/').pop()!;
+                unlink( path.join(__dirname, '..', '..', 'uploads', imageName), (err) => {
+                    if (err) {
+                        console.error(`Error deleting image: ${imagePathToDelete}`, err);
+                    } else {
+                        console.log(`Deleted image: ${imagePathToDelete}`);
+                    }
+                });
+            } else {
+                return res.status(400).json({
+                    status: 400,
+                    message: "Image not found in product!",
+                });
+            }
+
+            return res.status(200).json({
+                status: 200,
+                message: "Image deleted successfully!",
+            });
+        }
+        catch (error) {
+            console.error("error: ", error);
+            return res.status(500).json({
+                status: 500,
+                message: "Failed to delete image!",
+                error,
             });
         }
     }
--- app/routes/product.routes.ts
+++ app/routes/product.routes.ts
@@ -9,5 +9,6 @@ route.get('/get/products', productController.getAllProducts);
 route.get('/get/products/:id', productController.getProductById)
 route.put('/update/product/:id', isEmployee, upload.any(), productController.updateProduct);
 route.delete('/delete/product/:id', isEmployee, isManager, productController.softyDeleteProduct);
+route.post('/delete/product/image/:id', isEmployee, productController.deleteProductImage);
 
 export default route;
